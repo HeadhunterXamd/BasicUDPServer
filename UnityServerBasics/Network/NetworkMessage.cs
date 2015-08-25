d85893e2eef935e65da4678666c67251c0ae30e8
HeadhunterXamd <nielsvanschooten@gmail.com>
2015-08-26 00:07:19 +0200

    added struct for the messages

---
+++ UnityServerBasics/Network/NetworkMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityServerBasics.Network
+{
+	class NetworkMessage
+	{
+		private int UID { get; set; }
+		private int Sequence { get; set; }
+
+		/// <summary>
+		/// time for ttr calculation
+		/// </summary>
+		private DateTime Time { get; set; }
+
+	}
+}
